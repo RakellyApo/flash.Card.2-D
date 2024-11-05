da021a870c1995929ab141358ef9fb792ea3617b
RakellyApo <140725627+RakellyApo@users.noreply.github.com>
2024-11-05 11:26:03 +0000

    a

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ function criaCartao(categoria, pergunta, resposta) {
 <div class="cartao__conteudo">
                     <h3>${categoria}</h3>
                     <div class="cartao__conteudo__pergunta">
-                        <p>${Pergunta}</p>
+                        <p>${pergunta}</p>
                     </div>
                     <div class="cartao__conteudo__resposta">
-                        <p>${Resposta}</p>
+                        <p>${resposta}</p>
                     </div>
                 </div>
  `
