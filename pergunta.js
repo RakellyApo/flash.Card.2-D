f8f098172e9dd8aa1a41fc815173dbc1ce0d4140
RakellyApo <140725627+RakellyApo@users.noreply.github.com>
2024-11-05 11:39:53 +0000

    a

--- pergunta.js
+++ pergunta.js
@@ -1,22 +1,21 @@
 criaCartao(
-    'inglês'
-    'Traduza: Don’t be afraid to be who you are.'
+    'inglês',
+    'Traduza: Don’t be afraid to be who you are.',
     'Não tenha medo de ser quem você é!'
 )
 criaCartao(
-    'Matemática'
-    'Em um projeto para a construção de um cinema, os arquitetos estão avaliando a elação entre a quantidade de fileiras e a quantidade 
-    de cadeiras em cada fileira. O projeto inicial prevê uma sala para 304 pessoas. No caso de utilizarem 19 fileiras, o número de cadeiras por fileira será?'
+    'Matemática',
+    'Em um projeto para a construção de um cinema, os arquitetos estão avaliando a elação entre a quantidade de fileiras e a quantidade de cadeiras em cada fileira. O projeto inicial prevê uma sala para 304 pessoas. No caso de utilizarem 19 fileiras, o número de cadeiras por fileira será?',
     '16'
 )
 criaCartao(
-    'Português'
-    'Ache a palavra com erro de grafia: caranguejo ; beneficência ou prazeirosamente ;  adivinhar;'
+    'Português',
+    'Ache a palavra com erro de grafia: caranguejo ; beneficência ou prazeirosamente ;  adivinhar;',
     'prazeirosamente ; adivinhar'
 )
 criaCartao(
-    'história'
-    'foi responsável pela morte de milhares de judeus?'
+    'história',
+    'foi responsável pela morte de milhares de judeus?',
     'Adolf Hitler'
 )
 criaCartao(
